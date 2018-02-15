06afd02412491b3138dcf66233e4bd7d2cd4e9ea
Johan Öhlund <c15jod@cs.umu.se>
2018-02-15 15:28:48 +0100

    testing

---
+++ Haskell/DAG.hs
@@ -0,0 +1,14 @@
+--------------------------------------------------------------------------------
+-- | Own implementation af a directed acyclic graph (DAG).
+-- 
+-- Copyright  : (c) Johan Öhlund,
+--            : (c) Arvid Ernstsson,2018
+-- Maintainer : c15jod@cs.umu.se,
+--              c15aen@cs.umu.se
+--
+-- Project for the course Programspråk VT18, Umeå universitet.
+--------------------------------------------------------------------------------
+
+
+data DAG = DAG  Integer deriving (Eq, Show)
+
