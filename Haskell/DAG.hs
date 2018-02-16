6a2c350c18c4e134764bdb9ecf74ee919a3f6137
Johan Öhlund <c15jod@cs.umu.se>
2018-02-16 16:05:53 +0100

    Polymorf??

--- Haskell/DAG.hs
+++ Haskell/DAG.hs
@@ -10,16 +10,53 @@
 --------------------------------------------------------------------------------
 
 
-data DAG = Vertex (Integer ,DAG) deriving (Eq, Show)
+
+data Vertice a b= V{name::a,
+                    wV::b} deriving (Eq, Show)
+
+
+data Edge a b=  E {frV::(Vertice a b),
+                   toV::(Vertice a b),
+                    wE::b} deriving (Eq, Show)
 
 
 
 
---add_vertex
 
---add_edge
+empty :: DAG a b
+add_vertex :: a -> DAG a b -> DAG a b
+add_edge :: b -> DAG a b -> DAG a b
+
+newtype DAG a b= DAGImpl ([a],[b]) deriving (Eq, Show)
+
+empty = DAGImpl ([],[])
+add_vertex v1 (DAGImpl (v,e)) = DAGImpl ((v1:v),e)   
+add_edge   e1 (DAGImpl (v,e)) = DAGImpl (v,(e1:e))   
+
 
 --topological_ordering
 
 --weight_of_longest_path
 
+{-
+empty :: Stack a
+isEmpty :: Stack a -> Bool
+push :: a -> Stack a -> Stack a
+top :: Stack a -> a
+pop :: Stack a -> (a,Stack a)
+ 
+newtype Stack a = StackImpl [a] deriving(Show, Eq, Ord) -- opaque!
+empty = StackImpl []
+isEmpty (StackImpl s) = null s
+push x (StackImpl s) = StackImpl (x:s)
+top (StackImpl s) = head s
+pop (StackImpl (s:ss)) = (s,StackImpl ss)
+-}
+
+
+
+
+
+{-
+https://wiki.haskell.org/Abstract_data_type
+-}
--- Haskell/test.hs
+++ Haskell/test.hs
@@ -13,6 +13,38 @@ push x (StackImpl s) = StackImpl (x:s)
 top (StackImpl s) = head s
 pop (StackImpl (s:ss)) = (s,StackImpl ss)
 
-test :: Integer-> Integer-> Stack Integer-> Stack Integer 
-test 0 _ st= st
-test num item st= ((test) (num-1) item (push item st))  
+test :: (Num a)=>Integer-> a-> Stack a-> Stack a 
+test num item st
+    | num > 0 = ((test) (num-1) item (push item st))
+    | num < 0 = ((test) (num+1) item (push item st))
+    | num== 0 = st
+
+
+
+
+-- Typsynonym
+type StringPair = (String, String)
+-- Typinkapsling med deriverade typklassinstanser
+newtype NamnTyp =  Namn (String, String) deriving (Eq, Ord, Show)
+
+efternamn :: NamnTyp -> String
+efternamn (Namn (_,y)) = y
+
+-- Hade funkat för type NamnTyp = (String, String)
+-- efternamn = snd
+
+-- Enkel ny datatyp med värdekonstruktorer
+data WeekEndDay = Friday | Saturday | Sunday deriving (Eq, Ord, Show)
+
+-- Parametriserad, rekursiv datatyp
+data List' a = EmptyList | ListItem a (List' a) deriving (Eq, Ord, Show)
+
+-- Datatyp med flera olika distinkta värdekonstruktorer
+data UserType = NotAUser | User String Int | Uid Int | Username String
+
+-- En instans av typklassen Show
+instance Show UserType where
+  show (NotAUser) = "Not a user"
+  show (User x y) = x ++ " " ++ (show y)
+  show (Uid x)  = show x
+  show (Username x) = x
