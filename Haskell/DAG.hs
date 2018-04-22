fae568666a5c9585a5cc81af01d1d79dadfd096b
c15jodHome <joah0087@student.umu.se>
2018-04-22 21:04:31 +0200

    GG

--- Haskell/DAG.hs
+++ Haskell/DAG.hs
@@ -40,38 +40,23 @@ data DAG a= DAGImpl [Vertex a] [Edge a] deriving (Show)
 
 class (Eq a,Ord a) => CalcWeight a where 
     add :: (a -> a -> [a])
-    compare' :: ([a] -> [a]-> Ordering)
     sum' :: [a] -> [a]
 
 instance (CalcWeight [Char]) where
     add a1 a2 = [a1 ++ a2]
-    compare' a1 a2 = compare a1 a2
     sum' list = [concat list]
 
 instance CalcWeight Char where
     add a1 a2 = [a1] ++ [a2]
-    compare' a1 a2 = compare a1 a2 
     sum' list = list
 
 instance CalcWeight Integer where
     add a1 a2 = [(a1 + a2)]
-    compare' a1 a2 = if sum a1 > sum a2 then LT else GT
     sum' list = [sum list]
 
 instance CalcWeight Double where
     add a1 a2 = [(a1 + a2)]
-    compare' a1 a2 = if sum a1 > sum a2 then LT else GT
     sum' list = [sum list]
-
-instance (Show a) => Show (Vertex a) where
-    show  (V a b)= "(V "++ show a ++ " " ++ show b ++ ")"
-
-instance (Show a) => Show (Edge a) where
-    show  (E v1 v2 wt)= "(E "++ show v1 ++ " " ++ show v2 ++" "++ show wt++")"
-
-instance (Eq a) => Eq (Edge a) where
-    (==) (E v1 v2 wt1) (E v3 v4 wt2) = (v1 == v3) && (v2 == v4)
-    (/=) (E v1 v2 wt1) (E v3 v4 wt2) = (v1 /= v3) || (v2 /= v4)
 
 instance (Eq a) => Eq (Vertex a) where
     (==) (V a1 b1) (V a2 b2)= (a1 == a2)
@@ -83,16 +68,29 @@ instance (Ord a) => Ord (Vertex a) where
     (<=) (V a1 b1) (V a2 b2)= (<=) b1 b2
     compare (V a1 b1) (V a2 b2)= compare b1 b2
 
+instance (Eq a) => Eq (Edge a) where
+    (==) (E v1 v2 wt1) (E v3 v4 wt2) = (v1 == v3) && (v2 == v4)
+    (/=) (E v1 v2 wt1) (E v3 v4 wt2) = (v1 /= v3) || (v2 /= v4)
+
 instance (Ord a) => Ord (Edge a) where
     (>=) (E v1 v2 wt1) (E v3 v4 wt2)  = (>=) wt1 wt2
     (<=) (E v1 v2 wt1) (E v3 v4 wt2)  = (<=)  wt1 wt2
     (<)  (E v1 v2 wt1) (E v3 v4 wt2)  = (<)  wt1 wt2
     compare (E v1 v2 wt1) (E v3 v4 wt2)  = compare  wt1 wt2
+
+instance (Show a) => Show (Vertex a) where
+    show  (V a b)= "(V "++ show a ++ " " ++ show b ++ ")"
+
+instance (Show a) => Show (Edge a) where
+    show  (E v1 v2 wt)= "(E "++ show v1 ++ " " ++ show v2 ++" "++ show wt++")"
 -- ##################################################################################################################### 
 
-longestPath :: (CalcWeight a,Ord a) =>  [[a]] -> [a]
-longestPath list = head((sortBy) (compare') list)
-
+{-
+Function: longestPath
+Comment: Calculate the longest path of all possible paths.
+-}
+longestPath :: (CalcWeight a) =>  [[a]] -> [a]
+longestPath list = last((sortBy) (compare) list)
 
 {-
 Function: empty
@@ -117,7 +115,7 @@ Comment: Adds a edge between two vertices in the DAG. The new DAG (with the new 
 add_edge :: Eq a => DAG a -> VertID -> VertID -> a -> DAG a
 add_edge (DAGImpl v e) id1 id2 wt = 
                 if ((containsID v) id1) && ((containsID v) id2) 
-                    && (length((topological_ordering) (DAGImpl v ((E getV1 getV2 wt):e)) v [] ) > 0)
+                    && (length((topological_ordering) (DAGImpl v ((E getV1 getV2 wt):e))) > 0)
                 then (DAGImpl v ((E getV1 getV2 wt):e))
                 else error "Can't add edge, creats a cycle."
                     where   getV1 = getVert (DAGImpl v e) id1
@@ -125,14 +123,21 @@ add_edge (DAGImpl v e) id1 id2 wt =
 
 {-
 Function: topological_ordering
-Comment: Performs a topological sorting of the DAG and returns a sorted list of vertices.  
+Comment: Performs a topological sorting of the DAG and returns a sorted list of vertID:s.  
 -}
-topological_ordering :: Eq a => DAG a -> [Vertex a] -> [Vertex a] -> [Vertex a]
-topological_ordering (DAGImpl [] _) _ output  = output
-topological_ordering (DAGImpl _ _) [] _  = error "DAG is not acyclic!"
-topological_ordering (DAGImpl v e) (x2:xs2) output
-        | ((isToEdge) x2 e) == True = (topological_ordering) (DAGImpl v e) xs2 output
-        | otherwise = (topological_ordering) (DAGImpl fVertList fEdgeList) fVertList (x2:output)
+topological_ordering :: Eq a => DAG a -> [VertID]
+topological_ordering (DAGImpl v e) = map getVertId ((topological_ordering') (DAGImpl v e) v [])
+
+{-
+Function: topological_ordering'
+Comment: Help-function to topological_ordering.  
+-}
+topological_ordering' :: Eq a => DAG a -> [Vertex a] -> [Vertex a] -> [Vertex a]
+topological_ordering' (DAGImpl [] _) _ output  = reverse output
+topological_ordering' (DAGImpl _ _) [] _  = error "DAG is not acyclic!"
+topological_ordering' (DAGImpl v e) (x2:xs2) output
+        | ((isToEdge) x2 e) == True = (topological_ordering') (DAGImpl v e) xs2 output
+        | otherwise = (topological_ordering') (DAGImpl fVertList fEdgeList) fVertList (x2:output)
                     where fVertList = [res |res  <- v , ((/=) res  x2)]
                           fEdgeList = [res2|res2 <- e , ((/=) ((getFromVert)res2) x2)]
 
@@ -140,62 +145,35 @@ topological_ordering (DAGImpl v e) (x2:xs2) output
 Function: weight_of_longest_path
 Comment: Calculates the longest path between the two selected vertices in the DAG. 
 -}
-weight_of_longest_path :: (CalcWeight a1, Eq a) => DAG a -> VertID -> VertID -> (Vertex a -> a1)
-                                -> ([Edge a] -> Vertex a -> Vertex a -> a1)-> [a1]
-weight_of_longest_path dag id1 id2 f g = (sum' (longestPath(weight_of_longest_path' dag paths f g)))
+weight_of_longest_path :: (CalcWeight a) => DAG a -> VertID -> VertID ->
+                                                    (a -> a) -> (a -> a) -> [a]
+weight_of_longest_path dag id1 id2 f g = (longestPath(weight_of_longest_path' dag paths f g))
             where 
                 paths = (clrPaths) (getPaths dag vert2 getNeigh [vert1])
                 vert1 = getVert dag id1
                 vert2 = getVert dag id2
                 getNeigh = getNeighbours vert1 (getEdgeList dag) 
 
-
 {-
 Function: weight_of_longest_path'
 Comment: Help function to weight_of_longest_path that recursive calls weightOfPath to calculate
         weight of a path. 
 -}
-weight_of_longest_path' :: CalcWeight a => DAG a1 -> [[a2]] -> (a2 -> a) -> ([Edge a1] -> a2 -> a2 -> a) -> [[a]]
+weight_of_longest_path' :: (CalcWeight a) => DAG a -> [[Vertex a]] -> (a -> a) -> (a -> a) -> [[a]]
 weight_of_longest_path' _ [] _ _ = []
-weight_of_longest_path' dag (x:xs) f g= result : weight_of_longest_path' dag xs f g
+weight_of_longest_path' dag (x:xs) f g= sum' result : weight_of_longest_path' dag xs f g
                     where result = weightOfPath dag x f g
 
 {-
 Function: weightOfPath
 Comment: Calculates a path between vertices.
 -}
-weightOfPath :: CalcWeight a => DAG a1 -> [a2] -> (a2 -> a) -> ([Edge a1] -> a2 -> a2 -> a) -> [a]
-weightOfPath dag [] f g = []
-weightOfPath dag (x:[]) f g = [(f x)]
-weightOfPath dag (x:x1:xs) f g = 
-    ((add) (f x)  (g (getEdgeList dag) x x1)) 
-                            ++ weightOfPath dag (x1:xs) f g
-
-{-
-Function: calcCharWeight
-Comment: Calculates a paths weight (where the weights have the type Char).
--}
-calcCharWeight :: [a1] -> t -> (a1 -> a) -> (t -> a1 -> a1 -> a) -> [a]
-calcCharWeight (x:[]) eList f g = [(f x)]
-calcCharWeight (x:x1:xs) eList f g = ([(f x)] ++ [(g eList x x1)]) ++ calcCharWeight (x1:xs) eList f g
-
-{-
-Function: calcStringWeight
-Comment: Calculates a paths weight (where the weights have the type String).
--}
-calcStringWeight :: [a1] -> t -> (a1 -> [a]) -> (t -> a1 -> a1 -> [a]) -> [a]
-calcStringWeight (x:[]) eList f g = (f x)
-calcStringWeight (x:x1:xs) eList f g = (f x) ++ (g eList x x1) ++ calcStringWeight (x1:xs) eList f g
-
-{-
-Function: calcIntWeight
-Comment: Calculates a paths weight (where the weights have the type Integer).
--}
-calcIntWeight :: Num a => [a1] -> t -> (a1 -> a) -> (t -> a1 -> a1 -> a) -> [a]
-calcIntWeight (x:[]) eList f g = [f x]
-calcIntWeight (x:x1:xs) eList f g = (f x) + (g eList x x1) : calcIntWeight (x1:xs) eList f g
-
-
+weightOfPath :: (CalcWeight a) => DAG a -> [Vertex a] -> (a -> a) -> (a -> a) -> [a]
+weightOfPath dag ((V _ wt):[]) f g = [(f wt)]
+weightOfPath dag ((V i1 wt1):(V i2 wt2):xs) f g = 
+                ((add) (f wt1) (g edgeWeight)) 
+                        ++ weightOfPath dag ((V i2 wt2):xs) f g
+                where edgeWeight = (getEdgeWT) $ (getEdge) dag (V i1 wt1) (V i2 wt2)
 {-
 Function: isToEdge
 Comment: Returns True if the selected vertex has an edge that is pointed to the vertex else returns false.
@@ -206,7 +184,6 @@ isToEdge (V n1 wt1) ((E _ (V n2 wt2) wt):xs)
     | n1 == n2 = True
     | otherwise = isToEdge (V n1 wt1) xs
 
- 
 {-
 Function: getNewVID
 Comment: Returns the next free vertex identifier.
@@ -244,17 +221,6 @@ getEdge (DAGImpl v ((E v1 v2 wt):xs)) v3 v4
     | v1 == v3 && v2 == v4 = (E v1 v2 wt)
     | otherwise = getEdge (DAGImpl v xs) v3 v4
 
-
-{-
-Function: edgeWT
-Comment: Returns the weight of a selected edge from the DAG (if possible).
--}
-edgeWT :: (Eq a) => [Edge a]-> Vertex a -> Vertex a -> a
-edgeWT [] _ _ = error "Could not find edge in edgeWT"
-edgeWT (x:xs) v1 v2 = if ((getFromVert x) == v1) && ((getToVert x) == v2)  
-                            then (getEdgeWT x)
-                            else edgeWT xs v1 v2
-
 {-
 Function: getPaths
 Comment: Returns all possible paths from vertex a to vertex b.
@@ -284,11 +250,14 @@ clrPaths :: (Eq a) => [[Vertex a]] -> [[Vertex a]]
 clrPaths [] = []
 clrPaths (x:xs) = if x == [] then clrPaths xs else x :clrPaths xs
 
-getEdgeWT :: (Edge a) -> a
-getEdgeWT (E _ _ wt)= wt
+getEdgeWT :: Edge a -> a
+getEdgeWT (E _ _ wt) = wt
 
-vertWT :: (Vertex a) -> a
-vertWT (V _ wt)= wt
+edgeWT :: a -> a
+edgeWT wt = wt
+
+vertWT :: a -> a
+vertWT wt = wt
 
 getFromVert :: (Edge a) -> (Vertex a)
 getFromVert (E v1 _ _) = v1
